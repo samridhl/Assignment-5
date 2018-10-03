21fe779b5cdfa3d340b5f779637e5fdb376ea853
samridhl <tgrn510@github.com>
2018-10-03 00:19:37 +0000

    rc9

--- i.py
+++ i.py
@@ -15,16 +15,22 @@ for line in fileinput.input(['/data/Homo_sapiens.GRCh37.75.gtf']):
         if len(text_in_column)>3:
             if text_in_column[2] == "gene": 
                 gene_name_matches = re.findall('gene_name \"(.*?)\";', line)
-                
                 if user_gene_name == gene_name_matches[0]:
                     gene_id_matches = re.findall('gene_id \"(.*?)\";', line)
                     
 
-data = urllib.urlopen("http://rest.ensembl.org/overlap/id/ENSG00000130203.json?feature=variation")
+data = urllib.urlopen("http://rest.ensembl.org/overlap/id/"+ gene_id_matches[0] +".json?feature=variation")
 json_obj = json.load(data)
 
 for i in json_obj:
-    print  i["id"],i["consequence_type"],i["clinical_significance"]
+    id_names = i['id']
+    consequence_type = i['consequence_type']
+    consequence_new = consequence_type.replace("_"," ")
+    clinical_significance = i['clinical_significance']
+    if clinical_significance:
+        print "varaint" + id_names + "is a" + consequence_new  + "," + " and is clinically " + clinical_significance[0].upper()
+    else:
+        print "varaint" + id_names + "is a" + consequence_new  +  ","
    
     
 
